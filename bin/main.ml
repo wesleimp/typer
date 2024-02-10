763472d1c5ad95076ef4c385234a6f8c0955aef3
Weslei Juan Moser Pereira <wesleimsr@gmail.com>
2024-02-09 23:53:08 -0300

    feat: handle keydown for ascii letters and space

--- bin/main.ml
+++ bin/main.ml
@@ -9,31 +9,46 @@ let init _ = Command.Noop
 
 type model = {
   progress_bar : Progress.t;
-  words : string list;
   cursor : int;
+  text : string list;
+  typed : string list;
 }
 
+let explode_string s =
+  let chars = List.init (String.length s) (String.get s) in
+  List.map Char.escaped chars
+
 let initial_model =
-  let words = Words.random 15 in
-  let width = String.length (String.concat " " words) in
+  let words = Words.random 5 in
+  let text = explode_string (String.concat " " words |> String.trim) in
   {
     cursor = 0;
-    words = Words.random 15;
+    text;
+    typed = [];
     progress_bar =
-      Progress.make ~width ~color:(`Plain (Spices.color "#3fa2a3")) ();
+      Progress.make ~width:100
+        ~color:(`Plain (Spices.color "#3fa2a3"))
+        ~trail_char:" " ();
   }
+
+let handle_key_down m key =
+  let typed = m.typed @ [ key ] in
+  let amount =
+    float_of_int (List.length typed) /. float_of_int (List.length m.text)
+  in
+  let progress_bar = Progress.set_progress m.progress_bar amount in
+  {progress_bar; cursor = m.cursor + 1; typed; text = m.text}
 
 let update event m =
   match event with
-  | Event.KeyDown (Key "q" | Escape) -> (m, Command.Quit)
-  (* | Event.Frame _now -> *)
-  (*   let progress_bar = Progress.increment m.progress_bar 0.00001 in *)
-  (*   ({progress_bar;}, Command.Noop) *)
+  | Event.KeyDown Escape -> (m, Command.Quit)
+  | Event.KeyDown Space -> (handle_key_down m " ", Command.Noop)
+  | Event.KeyDown (Key k) -> (handle_key_down m k, Command.Noop)
   | _ -> (m, Command.Noop)
 
 let view m =
-  Format.sprintf "\n\n%s\n\n%s\n\n"
+  Format.sprintf "\n\n%s\n\n%s\n\n%s\n"
     (Progress.view m.progress_bar)
-    (String.concat " " m.words)
+    (String.concat "" m.text) (String.concat "" m.typed)
 
 let () = Minttea.app ~init ~update ~view () |> Minttea.start ~initial_model
